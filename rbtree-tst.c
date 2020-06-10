390c0eadf6198de8bd40ff613696094854b20a86
yiwei <yiwei.lh@alibaba-inc.com>
2020-06-10 14:50:28 +0800

    firt Version

--- rbtree-tst.c
+++ rbtree-tst.c
@@ -17,16 +17,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+
+struct over_speed_node{
+	uint32_t bandwidth_id;
+	uint64_t rx_over_speed;
+	uint64_t tx_over_speed;
+	uint8_t is_limited;
+}over_speed_node_t;
 
 struct mynode {
   	struct rb_node node;
-  	char *string;
+  	over_speed_node_t *over_speed_data;
 };
 
 struct rb_root mytree = RB_ROOT;
 
-struct mynode * my_search(struct rb_root *root, char *string)
+
+// compare function for sort
+static bool comp(over_speed_node_t *nodea,	over_speed_node_t *nodeb)
+{
+	if((nodea->is_limited == 0) && (nodeb->is_limited == 0)) 
+	{
+		return comp_help(nodea->rx_over_speed, nodea->tx_over_speed, \
+		nodeb->rx_over_speed, nodeb->tx_over_speed);
+	}
+  	else if(nodea->is_limited || nodeb->is_limited)
+	{
+		return nodea->is_limited ? -1 : 1;
+	}
+	else   //都已经派发了meter则如何排序？
+		return (nodea->bandwidth_id == nodeb->bandwidth_id) ? 0: (nodea->bandwidth_id < nodeb->bandwidth_id); 
+}
+
+static bool comp_help(uint64_t ar, uint64_t at, uint64_t br, uint64_t bt)
+{
+	uint64_t Max_value = max(ar, max(at, max(br, bt)));
+	if(Max_value == max(ar, at) && Max_value == max(br, bt))
+	{
+		return (nodea->bandwidth_id == nodeb->bandwidth_id) ? 0: (nodea->bandwidth_id < nodeb->bandwidth_id);
+	} 
+	else if(Max_value == max(ar, at))
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	} 
+}
+
+struct mynode * my_search(struct rb_root *root, over_speed_node_t *over_speed_data)
 {
   	struct rb_node *node = root->rb_node;
 
@@ -34,7 +74,7 @@ struct mynode * my_search(struct rb_root *root, char *string)
   		struct mynode *data = container_of(node, struct mynode, node);
 		int result;
 
-		result = strcmp(string, data->string);
+		result = comp(over_speed_data, data->over_speed_data);
 
 		if (result < 0)
   			node = node->rb_left;
@@ -53,7 +93,7 @@ int my_insert(struct rb_root *root, struct mynode *data)
   	/* Figure out where to put new node */
   	while (*new) {
   		struct mynode *this = container_of(*new, struct mynode, node);
-  		int result = strcmp(data->string, this->string);
+  		int result = comp(data->over_speed_data, this->over_speed_data);
 
 		parent = *new;
   		if (result < 0)
@@ -74,16 +114,16 @@ int my_insert(struct rb_root *root, struct mynode *data)
 void my_free(struct mynode *node)
 {
 	if (node != NULL) {
-		if (node->string != NULL) {
-			free(node->string);
-			node->string = NULL;
+		if (node->over_speed_data != NULL) {
+			free(node->over_speed_data);
+			node->over_speed_data = NULL;
 		}
 		free(node);
 		node = NULL;
 	}
 }
 
-#define NUM_NODES 32
+#define NUM_NODES 10
 
 int main()
 {
@@ -92,31 +132,50 @@ int main()
 
 	/* *insert */
 	int i = 0;
-	printf("insert node from 1 to NUM_NODES(32): \n");
+	//printf("insert node from 1 to NUM_NODES(32): \n");
 	for (; i < NUM_NODES; i++) {
 		mn[i] = (struct mynode *)malloc(sizeof(struct mynode));
-		mn[i]->string = (char *)malloc(sizeof(char) * 4);
-		sprintf(mn[i]->string, "%d", i);
+		mn[i]->over_speed_data = (over_speed_node_t *)malloc(sizeof(over_speed_node_t));
+		mn[i]->over_speed_data->bandwidth_id = 1000+i;
+		mn[i]->over_speed_data->tx_over_speed = 20-i;
+		mn[i]->over_speed_data->rx_over_speed = 20-i;
+		mn[i]->over_speed_data->is_limited = i>5? 1:0;
 		my_insert(&mytree, mn[i]);
 	}
 	
-	/* *search */
+	/* *print all node */
 	struct rb_node *node;
 	printf("search all nodes: \n");
 	for (node = rb_first(&mytree); node; node = rb_next(node))
-		printf("key = %s\n", rb_entry(node, struct mynode, node)->string);
+	{
+		printf("The rb_node : %u,%lu,%lu,%u\n", \
+		rb_entry(node, struct mynode, node)->over_speed_data->bandwidth_id, \
+		rb_entry(node, struct mynode, node)->over_speed_data->tx_over_speed, \
+		rb_entry(node, struct mynode, node)->over_speed_data->rx_over_speed, \
+		rb_entry(node, struct mynode, node)->over_speed_data->is_limited);
+	}
+
 
 	/* *delete */
-	printf("delete node 20: \n");
-	struct mynode *data = my_search(&mytree, "20");
+	printf("delete bandwidth_id 1002: \n");
+	over_speed_node_t *key = (over_speed_node_t *)malloc(sizeof(over_speed_node_t));
+	key->bandwidth_id = 1002;
+	key->tx_over_speed = 18;
+	key->rx_over_speed = 18;
+	key->is_limited = 0;
+	struct mynode *data = my_search(&mytree, key);
 	if (data) {
 		rb_erase(&data->node, &mytree);
 		my_free(data);
 	}
 
 	/* *delete again*/
-	printf("delete node 10: \n");
-	data = my_search(&mytree, "10");
+	printf("delete node 1006: \n");
+	key->bandwidth_id = 1006;
+	key->tx_over_speed = 14;
+	key->rx_over_speed = 14;
+	key->is_limited = 1;
+	data = my_search(&mytree, key);
 	if (data) {
 		rb_erase(&data->node, &mytree);
 		my_free(data);
@@ -133,7 +192,13 @@ int main()
 	/* *search again*/
 	printf("search again:\n");
 	for (node = rb_first(&mytree); node; node = rb_next(node))
-		printf("key = %s\n", rb_entry(node, struct mynode, node)->string);
+	{
+		printf("The rb_node : %u,%lu,%lu,%u\n", \
+		rb_entry(node, struct mynode, node)->over_speed_data->bandwidth_id, \
+		rb_entry(node, struct mynode, node)->over_speed_data->tx_over_speed, \
+		rb_entry(node, struct mynode, node)->over_speed_data->rx_over_speed, \
+		rb_entry(node, struct mynode, node)->over_speed_data->is_limited);
+	}
 	return 0;
 }
 
